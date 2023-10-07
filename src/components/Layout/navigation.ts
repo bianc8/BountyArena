c6d967b3b412f82fac002a3e8ced44f74dc4c0a4
bianc8.eth <48656276+bianc8@users.noreply.github.com>
2023-10-07 09:47:51 +0200

    update navigation names, added graphql, yarn.lock

--- src/components/Layout/navigation.ts
+++ src/components/Layout/navigation.ts
@@ -11,8 +11,8 @@ import {
 export const navigation = [
   { name: 'My board', href: '/dashboard', icon: UserIcon, current: false },
   { name: 'Chat', href: '/dashboard/messaging', icon: ChatBubbleBottomCenterIcon, current: false },
-  { name: 'Gigs', href: '/dashboard/services', icon: BriefcaseIcon, current: false },
-  { name: 'Talents', href: '/dashboard/talents', icon: UserGroupIcon, current: false },
+  { name: 'Bounties', href: '/dashboard/services', icon: BriefcaseIcon, current: false },
+  { name: 'Hackers', href: '/dashboard/talents', icon: UserGroupIcon, current: false },
 ];
 
 export const navigationAdmin = [
